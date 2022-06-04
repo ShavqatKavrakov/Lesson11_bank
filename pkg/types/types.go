5727c19020ef5629d9cca7aae746ae4c75ae4121
Shavqat <kavrakovshavqatinfo@gmail.com>
2022-06-04 16:43:22 +0300

    добовлена поля Status в Payment

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -6,9 +6,20 @@ type Money int64
 //Category представляет собой категорию, в которой был совершён платёж (авто, аптеки, рестораны и т.д).
 type Category string
 
+//Status представляет собой статус платежей.
+type Status string
+
+//Предопределённые статус платежей.
+const (
+	StatusOk         Status = "Ok"
+	StatusFail       Status = "Fail"
+	StatusInProgress Status = "INPROGRESS"
+)
+
 //Payment представлет информацию о платёже.
 type Payment struct {
 	ID       int
 	Amount   Money
 	Category Category
+	Status   Status
 }
